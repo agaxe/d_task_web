d5d95ede2414aba8608be8448ad41ccbc49b90de
agaxe <agaxe.dev@gmail.com>
2022-07-27 23:57:13 +0900

    test: build test (vercel)

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,21 @@
-//import adapter from '@sveltejs/adapter-node';
-import adapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		//adapter: adapter(),
-		//adapter: adapter({ out: 'my-output-directory' }),
-		adapter: adapter({
-			fallback: 'index.html'
-		}),
-		prerender: {
-			default: true
-		}
+		// default options are shown
+		adapter: vercel({
+			// if true, will deploy the app using edge functions
+			// (https://vercel.com/docs/concepts/functions/edge-functions)
+			// rather than serverless functions
+			edge: false,
+
+			// an array of dependencies that esbuild should treat
+			// as external when bundling functions
+			external: [],
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
+		})
 	}
 };
-
-export default config;
