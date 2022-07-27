dce1864bf3a11c271c0ea48d804c9124aa53f1df
agaxe <agaxe.dev@gmail.com>
2022-07-27 21:40:16 +0900

    test: build test

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
+//import adapter from '@sveltejs/adapter-node';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		//adapter: adapter(),
+		//adapter: adapter({ out: 'my-output-directory' }),
+		adapter: adapter({
+			fallback: 'index.html'
+		}),
 		prerender: {
 			default: true
 		}
