30ae1c0be0e8f1ffdaed31092fdc3b8c35bb7408
agaxe <agaxe.dev@gmail.com>
2022-07-29 00:09:30 +0900

    feat: Test 페이지 생성 및 SCSS 세팅

---
+++ src/routes/test.svelte
@@ -0,0 +1,16 @@
+<div class="content">
+  <h1 class="content__title">Test Page</h1>
+</div>
+
+<style lang="scss">
+  $color-red: #f00;
+  $color-orange : #00f;
+
+  //* test style
+  .content{
+    border:1px solid $color-red;
+    &__title{
+      color:$color-orange;
+    }
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,13 @@
 import vercel from '@sveltejs/adapter-vercel';
+import preprocessor from 'svelte-preprocess';
 
 export default {
-	kit: {
-		// default options are shown
-		adapter: vercel({
-			// if true, will deploy the app using edge functions
-			// (https://vercel.com/docs/concepts/functions/edge-functions)
-			// rather than serverless functions
-			edge: false,
-
-			// an array of dependencies that esbuild should treat
-			// as external when bundling functions
-			external: [],
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app
-			split: false
-		})
-	}
+  preprocess: preprocessor(),
+  kit: {
+    adapter: vercel({
+      edge: false,
+      external: [],
+      split: false
+    })
+  }
 };
