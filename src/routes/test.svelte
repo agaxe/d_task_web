ab12398bffd530701ed718a45abb8c19c4f0e4e0
agaxe <agaxe.dev@gmail.com>
2022-07-30 22:55:19 +0900

    update: [테스트] 페이지 - Test 컴포넌트 적용

--- src/routes/test.svelte
+++ src/routes/test.svelte
@@ -1,33 +1,16 @@
-<!-- <script lang="ts">
-    import App from '../App.svelte'
+<script lang="ts">
+  import { Test } from '@/components'
 </script>
 
-<App>
-  <div class="content">
+<div class="content">
   <h1 class="content__title">Test Page</h1>
-  <p class="title">title</p>
+  <Test/>
+  <p class="title">global title</p>
 </div>
-</App> -->
 
- <div class="content">
-  <h1 class="content__title">Test Page</h1>
-  <p class="title">title----</p>
- </div>
-
- 
 <style lang="scss">
-  $color-red: #f00;
-  $color-orange : #00f;
-
-  //* test style
   .content{
-    border:1px solid $color-red;
-    &__title{
-      color:$color-orange;
-    }
-  }
-
-  .title{
-    color:#00f
+    border:1px solid #f00;
   }
 </style>
+
