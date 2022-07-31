270af0d1d95d93357cb84cfd64e0eedcc0504346
agaxe <agaxe.dev@gmail.com>
2022-07-31 22:47:31 +0900

    feat: Seo 컴포넌트 생성
    - <title> 태그 적용

---
+++ src/components/Seo/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  const title = 'DTASK';
+  export let page: string = '';
+</script>
+
+<svelte:head>
+  <title>{page ? `${page} | ${title}` : title}</title>
+</svelte:head>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { default as Test } from './Test/index.svelte';
+export { default as Seo } from './Seo/index.svelte';
