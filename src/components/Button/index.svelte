737a3b8d0aecbc1375cb17f49bf2ff0697139e85
agaxe <agaxe.dev@gmail.com>
2022-08-05 23:47:02 +0900

    feat: Button 컴포넌트 생성

---
+++ src/components/Button/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let className: string = 'button';
+</script>
+
+<button class={`button ${className}`}>
+  <p><slot /></p>
+</button>
+
+<style lang="scss">
+  @use './index';
+</style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Test } from './Test/index.svelte';
 export { default as Seo } from './Seo/index.svelte';
+export { default as Button } from './Button/index.svelte';
