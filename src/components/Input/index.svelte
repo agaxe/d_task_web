c280ab4cb11dbff6cedf31e7417d0bfa83baa7a0
agaxe <agaxe.dev@gmail.com>
2022-08-13 23:56:59 +0900

    feat: Input 컴포넌트 생성

---
+++ src/components/Input/index.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let id = '';
+  export let name = '';
+  export let type = 'text';
+  export let value = '';
+  export let maxLength = 0;
+  export let placeholder = '';
+  export let readOnly = false;
+  export let onChange = (e: any) => {};
+</script>
+
+<input
+  class="input"
+  {id}
+  {name}
+  {type}
+  {value}
+  {placeholder}
+  maxlength={maxLength}
+  readonly={readOnly}
+  on:change={onChange}
+/>
+
+<style lang="scss">
+  @use './index';
+</style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Test } from './Test/index.svelte';
 export { default as Seo } from './Seo/index.svelte';
 export { default as Button } from './Button/index.svelte';
+export { default as Input } from './Input/index.svelte';
