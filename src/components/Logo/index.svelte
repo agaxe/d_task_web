4f74e05d73d3481da2ec452c8d1e795eea24bfaf
agaxe <agaxe.dev@gmail.com>
2022-08-19 18:53:19 +0900

    feat: Logo 컴포넌트 생성

---
+++ src/components/Logo/index.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Icon } from '@/components';
+
+  const logoIconName = 'logo';
+  export let isLink: boolean = true;
+</script>
+
+<div class={`logo ${$$restProps.class || ''}`}>
+  {#if isLink}
+    <a href="/">
+      <Icon name={logoIconName} />
+    </a>
+  {:else}
+    <Icon name={logoIconName} />
+  {/if}
+</div>
+
+<style lang="scss">
+  @use './index';
+</style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Seo } from './Seo/index.svelte';
 export { default as Button } from './Button/index.svelte';
 export { default as Input } from './Input/index.svelte';
 export { default as Icon } from './Icon/index.svelte';
+export { default as Logo } from './Logo/index.svelte';
