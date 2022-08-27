b02539dc31c66c1b28822f7a0bf65b3681db956f
agaxe <agaxe.dev@gmail.com>
2022-08-27 17:31:39 +0900

    feat: Profile 컴포넌트 생성

---
+++ src/components/Profile/index.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Image, Icon } from '@/components';
+
+  export let src: string = '';
+  export let size: 'md' | 'lg' | '' = '';
+</script>
+
+<div class={`profile ${$$restProps.class || ''} size-${size}`}>
+  {#if src}
+    <Image class="profile-img" {src} alt="profile-img" />
+  {:else}
+    <Icon name="profile-empty" />
+  {/if}
+</div>
+
+<style lang="scss">
+  @use './index';
+</style>
