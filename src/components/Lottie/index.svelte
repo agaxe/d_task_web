b1fa885ccf2274e219e89aa0d293aa7f96388d98
agaxe <agaxe.dev@gmail.com>
2022-08-27 18:24:53 +0900

    feat: Lottie 컴포넌트 생성
    - LottieFiles 라이브러리를 사용한 컴포넌트를 생성하였습니다.

---
+++ src/components/Lottie/index.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import lottie from 'lottie-web';
+
+  export let id: string;
+  export let renderer: 'svg' | 'canvas' | 'html' = 'svg';
+  export let loop: boolean = false;
+  export let autoplay: boolean = true;
+  export let path: string;
+
+  let lottieElement: HTMLDivElement;
+
+  onMount(() => {
+    lottie.loadAnimation({
+      container: lottieElement,
+      renderer,
+      loop,
+      autoplay,
+      path
+    });
+  });
+</script>
+
+<div {id} class={`${$$restProps.class || ''}`} bind:this={lottieElement} />
--- src/components/index.ts
+++ src/components/index.ts
@@ -9,3 +9,4 @@ export { default as Image } from './Image/index.svelte';
 export { default as Chip } from './Chip/index.svelte';
 export { default as MessageBox } from './MessageBox/index.svelte';
 export { default as Profile } from './Profile/index.svelte';
+export { default as Lottie } from './Lottie/index.svelte';
