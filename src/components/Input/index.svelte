def40f93f3ee57c377073efe8e596ffa1fa39fad
agaxe <agaxe.dev@gmail.com>
2022-08-29 23:18:44 +0900

    update: Input 컴포넌트 - props 및 스타일 변경

--- src/components/Input/index.svelte
+++ src/components/Input/index.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
-  export let id = '';
-  export let name: string = '';
-  export let type: string = 'text';
+  // export let id = '';
+  // export let name: string = '';
+  // export let type: string = 'text';
   export let value: string | number = '';
-  export let placeholder: string = '';
-  export let maxLength: number = 9999;
-  export let readOnly: boolean = false;
-  export let pattern: string | undefined = undefined;
+  // export let placeholder: string = '';
+  // export let maxLength: number = 9999;
+  // export let readOnly: boolean = false;
+  // export let pattern: string | undefined = undefined;
 
-  function typeAction(node: HTMLInputElement) {
-    node.type = type;
-  }
+  // function typeAction(node: HTMLInputElement) {
+  //   node.type = type;
+  // }
+
+  interface $$Props
+    extends svelte.JSX.HTMLAttributes<HTMLElementTagNameMap['input']> {}
 </script>
 
-<input
+<input {...$$restProps} class={`input ${$$restProps.class}`} bind:value />
+
+<!-- <input
   class={`input ${$$restProps.class}`}
   {id}
   {name}
@@ -23,8 +28,7 @@
   maxlength={maxLength}
   readonly={readOnly}
   {pattern}
-/>
-
+/> -->
 <style lang="scss">
   @use './index';
 </style>
