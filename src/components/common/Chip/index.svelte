e58c9b24cad21c7d466be320037a12a0cb604578
agaxe <agaxe.dev@gmail.com>
2022-09-21 23:22:25 +0900

    update: Chip 컴포넌트 - iconName props 추가 및 스타일 수정

--- src/components/common/Chip/index.svelte
+++ src/components/common/Chip/index.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
   import { ColorThemeType } from '@/shared/type';
+  import { Icon } from '@/components';
+
   export let theme: ColorThemeType = 'purple';
   export let text: string;
+  export let iconName: string = '';
 </script>
 
-<div class={`chip ${theme} ${$$restProps.class || ''}`}>{text}</div>
+<div class={`chip ${theme} ${$$restProps.class || ''}`}>
+  <p>{text}</p>
+  {#if iconName}
+    <Icon class="icon" name={iconName} />
+  {/if}
+</div>
 
 <style lang="scss">
   @use './index';
